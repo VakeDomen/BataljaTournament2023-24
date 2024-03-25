ddb2e4b3b96fcd12b2e3b663c2bd7fca6e19f085
VakeDomen <vake.domen94@gmail.com>
2024-03-25 13:24:49 +0100

    overarching turny acript

---
+++ tournament.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+qualifiers=0
+turny=1
+
+if [ $qualifiers -eq 1 ]; then
+    clear
+    python3 display_banner.py "Qualifier groups:"
+    python3 display_groups.py
+    read -p "press [enter] to contune to qualifiers"
+
+    clear
+    ./quallifiers.sh
+fi
+
+if [ $turny -eq 1 ]; then
+    clear
+    python3 extract_win_rates.py
+    read -p "press [enter] to contune to eliminations"
+    rm brackets.txt
+    cp brackets_base.txt brackets.txt
+    clear
+    echo
+    python3 display_brackets.py
+fi
