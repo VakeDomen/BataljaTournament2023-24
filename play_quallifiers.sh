2c90caeace964ec6f7ac109576dc95e9dcfc1ba1
VakeDomen <vake.domen94@gmail.com>
2024-03-25 10:59:18 +0100

    one game pair in quallifiers

--- play_quallifiers.sh
+++ play_quallifiers.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
-
 game=0
 
-for group in G1 G2 G3 G4
-do 
+for group in G2 G3 G4; do
     echo "------------ $group -------------"
-    rm -r quallifiers_games/$group
-    mkdir quallifiers_games/$group
+    rm -rf quallifiers_games/$group
+    mkdir -p quallifiers_games/$group
 
+    team_list=($(ls quallifiers/$group))  # Store team names in an array
 
-    for team1 in $(ls quallifiers/$group)
-    do
+    for (( i=0; i<${#team_list[@]}; i++ )); do
+        team1=${team_list[$i]}
         
-        
-        for team2 in $(ls quallifiers/$group)
-        do
-            if [ $team1 = $team2 ]; then
-                continue
-            fi        
+        for (( j=i+1; j<${#team_list[@]}; j++ )); do  # Start from the next team
+            team2=${team_list[$j]}
 
-
-            while [ 1 ]
-            do
+            while true; do
                 clear
-                python3 display_brackets.py
+                # python3 display_brackets.py
+                python3 display_banner.py "Group: $group"
+                python3 display_banner.py ""
                 python3 display_banner.py "$team1"
                 python3 display_banner.py "VS"
                 python3 display_banner.py "$team2"
 
                 java -jar Game.jar quallifiers/$group/$team1/bot1 quallifiers/$group/$team1/bot2 quallifiers/$group/$team2/bot1 quallifiers/$group/$team2/bot2 > game.log
                 
-                read -p "write something to accept the game" key
+                read -p "write something to accept the game (1 to accept): " key
                 if [ "$key" = "1" ]; then
-                    mv game.log quallifiers_games/$group/$team1-$team2.log
+                    mv game.log quallifiers_games/$group/${team1}-${team2}.log
                     break
                 fi
             done
